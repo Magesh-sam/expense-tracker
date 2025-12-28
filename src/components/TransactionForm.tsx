1682aa51815bbb100d0cd68fd789f29e53a008f1
mageshkannan <mageshkannanam@gmail.com>
2025-12-28 10:24:06 +0530

    add: mandatory indication in form

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -43,9 +43,10 @@ const TransactionForm = ({ ref }: { ref: RefObject<HTMLDialogElement | null> }) 
 
     return (
         <form onSubmit={handleSubmit} className=" my-3 p-3 bg-white">
+            <p className="text-gray-600 "> * are mandatory</p>
             <div className="flex flex-col gap-3">
 
-                <p className="font-semibold">Transaction Type</p>
+                <p className="font-semibold">Transaction Type: *</p>
                 <div className="flex justify-evenly flex-wrap">
 
                     <label htmlFor="income" className="flex items-center gap-2" >
@@ -59,25 +60,25 @@ const TransactionForm = ({ ref }: { ref: RefObject<HTMLDialogElement | null> }) 
                     </label>
                 </div>
             </div>
-            <label className="flex items-center gap-5 justify-between flex-wrap mt-2" htmlFor="amount"><span className="font-semibold">Amount:</span>
+            <label className="flex items-center gap-5 justify-between flex-wrap mt-2" htmlFor="amount"><span className="font-semibold">Amount: *</span>
 
                 <input className=" p-2 border border-blue-500 rounded-sm" type="number" min={1} value={transaction.amount} onChange={handleChange} name="amount" id="amount" />
             </label>
-            <label className="flex items-center gap-5 justify-between mt-2" htmlFor="amount"><span className="font-semibold">Date:</span>
+            <label className="flex items-center gap-5 justify-between mt-2" htmlFor="amount"><span className="font-semibold">Date: *</span>
 
                 <input className=" p-2 border border-blue-500 rounded-sm" type="date" value={new Date().toISOString().split("T")[0]}
                     onChange={handleChange} name="date" id="date" />
             </label>
             <select required name="category" id="category" className="border border-blue-500 rounded-sm p-2 my-2 w-full "
                 value={transaction.category} onChange={handleChange}  >
-                <option value="">Category</option>
+                <option value="">Category *</option>
                 <option value="grocery">grocery</option>
                 <option value="medical">medical</option>
                 <option value="travel">travel</option>
                 <option value="others">others</option>
             </select>
             <label className="flex flex-col gap-2" htmlFor="amount"><span className="font-semibold">Description</span>
-                <textarea value={transaction.description} placeholder="fruits and vegetables from instamart" className="border border-blue-500 rounded-sm placeholder:p-1 " rows={3} name="description" id="description" onChange={handleChange} />
+                <textarea value={transaction.description} placeholder="fruits and vegetables from instamart" className="border border-blue-500 rounded-sm placeholder:p-1 p-2 " rows={3} name="description" id="description" onChange={handleChange} />
             </label>
             <div className="flex gap-3 mt-3 text-white">
                 <button className="px-3 py-2 bg-gray-400 hover:bg-gray-500 hover:cursor-pointer rounded-sm" onClick={handleCancel} type="button">Cancel</button>
